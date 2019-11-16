f1ef1a6c4297ed4177a63da1f10e094a4d5266e9
ziyaliuye <171242039@qq.com>
2019-11-16 23:44:24 +0800

    初始提交...

---
+++ src/test/java/cn/cnm/demo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cnm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
