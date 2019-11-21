39f1c06c0ba67b39cbf8c69a3b7a41f126631cdd
ziyaliuye <171242039@qq.com>
2019-11-22 00:30:55 +0800

    完善代码...

---
+++ src/main/java/cn/cnm/component/listener/MqListener.java
@@ -0,0 +1,32 @@
+package cn.cnm.component.listener;
+
+import cn.cnm.pojo.Flower;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lele
+ * @version 1.0
+ * @Description RabbitMQ监听器
+ * @Email 414955507@qq.com
+ * @date 2019/11/22 0:25
+ */
+// @Component
+public class MqListener {
+    /* 消息监听， queues属性对应一个数组， 可以监听多个队列 */
+    // @RabbitListener(queues = "cn.news")
+    public void receive(Flower flower) {
+        // 队列的消息会自动转换
+        System.out.println("接收到消息：" + flower);
+    }
+
+    /* 还可以获取消息实例， 从中获取消息头信息 */
+    // @RabbitListener(queues = "cnm.emps")
+    public void receiveMessage(Message message) {
+        if (message != null) {
+            System.out.println("消息头信息：：" + message.getMessageProperties());
+            System.out.println("消息体信息：" + message);
+        }
+    }
+}
